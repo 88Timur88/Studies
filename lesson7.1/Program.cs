3d93c10394d745afbc37da7c94ba2fb7019c1ec0
Fox <gazizov-1988@list.ru>
2023-02-24 21:11:52 +0500

    lesson7

---
+++ lesson7.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+
+int[,]matr = new int [,]
+{
+    6, 5, 4,
+    3, 2, 1
+};
+int max = 0;
+int min = 0;
+
+void CoolArray(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.WriteLine();
+        }
+    }
+}
